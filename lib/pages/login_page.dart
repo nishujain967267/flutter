d14fcb4a121c2dc33bab741a2e7b9327fb3e19ca
nishujain967267 <pranjalnishu08@gmail.com>
2021-05-20 17:02:30 +0530

    day 6 completed animation.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:learn/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changedButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -19,7 +26,7 @@ class LoginPage extends StatelessWidget {
               ),
               Center(
                 child: Text(
-                  "Welcome",
+                  "Welcome  $name",
                   style: TextStyle(
                     fontSize: 28,
                     fontWeight: FontWeight.bold,
@@ -35,9 +42,12 @@ class LoginPage extends StatelessWidget {
                 child: Column(
                   children: [
                     TextFormField(
-                      decoration: InputDecoration(
-                          hintText: "Enter User Name", labelText: "UserName"),
-                    ),
+                        decoration: InputDecoration(
+                            hintText: "Enter User Name", labelText: "UserName"),
+                        onChanged: (value) {
+                          name = value;
+                          setState(() {});
+                        }),
                     TextFormField(
                       obscureText: true,
                       decoration: InputDecoration(
@@ -52,19 +62,52 @@ class LoginPage extends StatelessWidget {
                     //     },
                     //     child: Text("login"))
 
-                    RawMaterialButton(
-                      onPressed: () {
+                    // RawMaterialButton(
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //     // routes.dart se use kiya hai homeroute ko jo ki phle app.js me hai
+                    //   },
+                    //   elevation: 2.0,
+                    //   fillColor: Colors.deepPurple,
+                    //   child: Text(
+                    //     "login",
+                    //     style: TextStyle(fontSize: 15, color: Colors.white),
+                    //   ),
+                    //   padding: EdgeInsets.all(15.0),
+                    //   shape: CircleBorder(),
+                    // )
+
+                    InkWell(
+                      onTap: () async {
+                        setState(() {
+                          changedButton = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
                         Navigator.pushNamed(context, MyRoutes.homeRoute);
-                        // routes.dart se use kiya hai homeroute ko jo ki phle app.js me hai
                       },
-                      elevation: 2.0,
-                      fillColor: Colors.deepPurple,
-                      child: Text(
-                        "login",
-                        style: TextStyle(fontSize: 15, color: Colors.white),
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changedButton ? 50 : 150,
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: changedButton
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18),
+                              ),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurpleAccent,
+                          borderRadius:
+                              BorderRadius.circular(changedButton ? 50 : 8),
+                        ),
                       ),
-                      padding: EdgeInsets.all(15.0),
-                      shape: CircleBorder(),
                     )
                   ],
                 ),
